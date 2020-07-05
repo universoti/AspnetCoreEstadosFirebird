3ded4c6c8845e6af9227a2dd7e1e4093aa7039d7
universoti <64614511+universoti@users.noreply.github.com>
2020-07-05 14:48:18 -0300

    terceito comitt

--- webestados/webestados/Data/Banco/Banco.cs
+++ webestados/webestados/Data/Banco/Banco.cs
@@ -11,8 +11,7 @@ namespace webestados.Data.Banco
     {
         private static string pUser = "SYSDBA";
         private static string pPassword = "masterkey";
-        private static string pDatabase = "localhost:B:\\PROJETOS_ERICK_2019\\ForcaVendas\\dados\\ForcaVendasBD.fdb";
-        private static string pDataSource = "localhost";
+        private static string pDatabase = "localhost:B:\\PROJETOS_ERICK_2019\\ForcaVendas\\dados\\ForcaVendasBD.fdb";        
         private static int pPort = 3050;
         private static int pDialect = 3;
         private static string pCharset = FbCharset.Utf8.ToString();
